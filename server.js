1a4b2bf0c862237813ebc6e0c5838f9463338fbc
rosenrose <crazytempler@gmail.com>
2022-06-30 10:41:06 +0900

    query & mutation type, non nullable

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+  }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+
   type Query {
-    text: String
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
 const server = new ApolloServer({ typeDefs });
 
 server.listen().then((args) => {
-  console.log(args);
+  // console.log(args);
 });
