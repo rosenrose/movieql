8a2b4d187e802963d73041c1998e7856f96616d3
rosenrose <crazytempler@gmail.com>
2022-06-30 13:08:27 +0900

    query & mutaion resolvers

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  { id: "0", text: "hello" },
+  { id: "1", text: "world" },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -8,7 +13,7 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
 
   type Query {
@@ -21,7 +26,39 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = { text, id: String(tweets.length) };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const index = tweets.findIndex((tweet) => tweet.id === id);
+      if (index < 0) {
+        return false;
+      }
+      tweets.splice(index, 1);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ res }) => {
+    res.header("Access-Control-Allow-Origin", "*");
+  },
+});
 
 server.listen().then((args) => {
   // console.log(args);
