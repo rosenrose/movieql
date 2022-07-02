da3a9d6cc993d364e0a9c554a790ac0aa8e17928
rosenrose <crazytempler@gmail.com>
2022-07-02 10:35:17 +0900

    migration

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import { ApolloServer, gql } from "apollo-server";
+import axios from "axios";
+import "dotenv/config";
+
+const API_KEY = process.env.API_KEY;
 
 const tweets = [
   { id: "0", text: "hello", userId: "aa" },
@@ -32,6 +36,7 @@ const typeDefs = gql`
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
     allUsers: [User!]!
+    allMovies: Movies!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
@@ -40,6 +45,58 @@ const typeDefs = gql`
     """
     deleteTweet(id: ID!): Boolean!
   }
+
+  type Movies {
+    kind: String!
+    etag: String!
+    nextPageToken: String!
+    items: [Item]!
+    pageInfo: PageInfo!
+  }
+  type PageInfo {
+    totalResults: Int!
+    resultsPerPage: Int!
+  }
+  type Item {
+    kind: String!
+    etag: String!
+    id: String!
+    snippet: Snippet!
+    contentDetails: ContentDetails!
+  }
+  type Snippet {
+    publishedAt: String!
+    channelId: String!
+    title: String!
+    description: String!
+    thumbnails: Thumbnails!
+    channelTitle: String!
+    playlistId: String!
+    position: Int!
+    resourceId: ResourceId!
+    videoOwnerChannelTitle: String!
+    videoOwnerChannelId: String!
+  }
+  type Thumbnails {
+    default: Thumbnail!
+    medium: Thumbnail
+    high: Thumbnail
+    standard: Thumbnail
+    maxres: Thumbnail
+  }
+  type Thumbnail {
+    url: String!
+    width: Int!
+    height: Int!
+  }
+  type ResourceId {
+    kind: String!
+    videoId: String!
+  }
+  type ContentDetails {
+    videoId: String!
+    videoPublishedAt: String!
+  }
 `;
 
 const resolvers = {
@@ -47,6 +104,13 @@ const resolvers = {
     allTweets: () => tweets,
     tweet: (root, { id }) => tweets.find((tweet) => tweet.id === id),
     allUsers: () => users,
+    allMovies: () =>
+      axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=UUyWiQldYO_-yeLJC0j5oq2g&key=${API_KEY}&part=snippet,contentDetails&maxResults=20`,
+          { responseType: "json" }
+        )
+        .then((r) => r.data),
   },
   Mutation: {
     postTweet: (_, { text, userId }) => {
